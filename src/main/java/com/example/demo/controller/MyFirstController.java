e9e32939f46d1bb0bf28552994e04fdfe8ae70bc
onnawarathna93 <onnawarathna93@gmail.com>
2026-02-07 08:51:56 +0530

    Simple get method added

--- src/main/java/com/example/demo/controller/MyFirstController.java
+++ src/main/java/com/example/demo/controller/MyFirstController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class MyFirstController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
