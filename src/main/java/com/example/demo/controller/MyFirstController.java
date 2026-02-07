2cdc65fe7820136fc04f2e1966d7435c9ea38ae4
chanuka <chanuucsc@gmail.com>
2026-02-07 16:48:04 +0530

    Feat : random changed added.

--- src/main/java/com/example/demo/controller/MyFirstController.java
+++ src/main/java/com/example/demo/controller/MyFirstController.java
@@ -1,13 +1,25 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequiredArgsConstructor
 public class MyFirstController {
+
+    private final RandomTest randomTest;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World! updated by Oshani.!";
+    }
+
+    @GetMapping("/random")
+    public String Random(@RequestParam String name, @RequestParam int value) {
+        randomTest.setName(name);
+        randomTest.setValue(value);
+        return randomTest.sayHello();
     }
 }
