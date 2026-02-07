e9e3748ad5fd06fcccde2710880adce89b88871c
onnawarathna93 <onnawarathna93@gmail.com>
2026-02-07 10:56:34 +0530

    Simple get method updated

--- src/main/java/com/example/demo/controller/MyFirstController.java
+++ src/main/java/com/example/demo/controller/MyFirstController.java
@@ -8,6 +8,6 @@ public class MyFirstController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World! updated by Oshani.!";
     }
 }
