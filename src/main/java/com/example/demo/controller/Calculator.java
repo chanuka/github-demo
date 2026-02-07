6163bbb2de96499bc0187c0c834c3b5b78d0dc3d
onnawarathna93 <onnawarathna93@gmail.com>
2026-02-07 16:55:57 +0530

    feat : calculator added

---
+++ src/main/java/com/example/demo/controller/Calculator.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@Data
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public int add() {
+        return a + b;
+    }
+
+    public int sub() {
+        return a - b;
+    }
+
+    public int mul() {
+        return a * b;
+    }
+
+    public double div() {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return ((double) a) / ((double) b);
+    }
+
+}
--- src/main/java/com/example/demo/controller/MyFirstController.java
+++ src/main/java/com/example/demo/controller/MyFirstController.java
@@ -9,11 +9,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class MyFirstController {
 
+    private final Calculator calculator;
+
     private final RandomTest randomTest;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World! updated by Oshani.!";
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int a, @RequestParam int b) {
+        try {
+            calculator.setA(a);
+            calculator.setB(b);
+            return String.format(
+                "Addition: %d, Subtraction: %d, Multiplication: %d, Division: %.4f",
+                    calculator.add(), calculator.sub(), calculator.mul(), calculator.div()
+            );
+        } catch (ArithmeticException ex) {
+            return String.format(
+                "Addition: %d, Subtraction: %d, Multiplication: %d, Division: error (%s)",
+                    calculator.add(), calculator.sub(), calculator.mul(), ex.getMessage()
+            );
+        }
     }
 
     @GetMapping("/random")
