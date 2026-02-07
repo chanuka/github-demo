fb04eebdafbd6b10f289e39c4325136c2fa539bb
onnawarathna93 <onnawarathna93@gmail.com>
2026-02-07 23:18:21 +0530

    feat : refactor the code second time

--- src/main/java/com/fortunaglobal/cargillsapp/controller/MyFirstController.java
+++ src/main/java/com/fortunaglobal/cargillsapp/controller/MyFirstController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.fortunaglobal.cargillsapp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestParam;
